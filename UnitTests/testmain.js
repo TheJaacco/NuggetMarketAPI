d691e78f72b9a95bca8a2b352d8b47b30cd2c846
Jaakko <jaakko-l@hotmail.com>
2020-10-02 01:52:51 +0300

    Date:      Fri Oct 2 01:52:51 2020 +0300
    On branch Jaakkodev
    
    Added body for unit testing. next task installing chai
    
    Changes to be committed:
    	modified:   UnitTests/testmain.js
    	modified:   package-lock.json
    	modified:   package.json

--- UnitTests/testmain.js
+++ UnitTests/testmain.js
@@ -1,1 +1,15 @@
-// teehee its empty
+let mocha = require('mocha');
+let server =  require('../src/index.js'); // or just index 
+
+describe('Task API' , () => {
+    //Testing GET
+    describe("GET /", () => {
+        it("it should get data", (done) => {
+            
+        })
+    })
+
+
+
+    //Testing POST
+});
