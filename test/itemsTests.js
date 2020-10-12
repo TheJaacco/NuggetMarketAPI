f138de958d8178867fcf80a16f3301fef9262ad0
Tahkamura <tonipuha91@gmail.com>
2020-10-12 12:44:25 +0300

    Added Tests for items POST,PUT and DELETE

--- test/itemsTests.js
+++ test/itemsTests.js
@@ -2,9 +2,13 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const server = require('../server');
+const { v4: uuidv4 } = require('uuid');
 
 const expect = chai.expect;
 const apiAddress = 'http://localhost:3000';
+
+let firtsItemId = "";
+let putAddres = "";
 
 // -------------- tests for GET ------------------------
 describe('Test get all items', function() {
@@ -12,10 +16,6 @@ describe('Test get all items', function() {
     before(function(){
         server.start();
     });
-
-    after(function(){
-        server.stop();
-    })
 
     describe('Get statuscode', function() {
         it("Should respond with statuscode 200", async function() {
@@ -32,6 +32,8 @@ describe('Test get all items', function() {
             await chai.request(apiAddress)
             .get('/items')
             .then(response => {
+                //console.log(response.body); Uncommet if you want to see items printed and see if test really works
+                firtsItemId = response.body.items[0].id;
                 expect(response.body.items[0]).to.have.a.property('id');
             })
             .catch(error => {
@@ -51,3 +53,109 @@ describe('Test get all items', function() {
     });
 });
 // --------------Test for POST--------------------------------
+describe('Post new item', function() {
+    it('Should post new item', async function() {
+      await chai.request(apiAddress)
+        .post('/items')
+        .send({
+                id: uuidv4(),
+                Title: "Ilmaa",
+                Description: "Vähän höngittyä",
+                Category: "Oleellinen",
+                Location: {
+                 City: "Turku",
+                  Country: "Suomi",
+                  PostalCode: "815"
+                },
+                Images: "Ilmaa",
+                AskingPrice: "10e",
+                DateOfPosting: "Eilen",
+                DeliveryType: "Posti",
+                SellerName: "Seppo Sebastian",
+                ContactInformation: "0400123123",
+        })
+        .then(response => {
+          expect(response.status).to.equal(200);
+          return chai.request(apiAddress).get('/items');
+        })
+        .catch(error => {
+          expect.fail(error)
+        })
+    })
+    // Uncommet if you want to see items printed and see if test really works
+    /*it("Print all items", async function() {
+        await chai.request(apiAddress)
+        .get('/items')
+        .then(response => {
+            console.log(response.body);
+        })
+        .catch(error => {
+            expect.fail(error)
+        })
+     })*/
+  })
+//-------------------------- Test for PUT----------------------------------------------------------------
+
+describe('Modify first item', function() {
+    before(function() {
+        putAddres = "/items/" + firtsItemId;
+      })
+    it('Should modify asking price ', async function() {
+      await chai.request(apiAddress)
+        .put(putAddres)
+        .send({
+            AskingPrice: "13e"
+        })
+        .then(response => {
+          expect(response.status).to.equal(200);
+          return chai.request(apiAddress).get('/items/1');
+        })
+        .catch(error => {
+          expect.fail(error)
+        })
+    })
+    // Uncomment if you want to see items printed and see if test really works
+    /*it("Print all items", async function() {
+        await chai.request(apiAddress)
+        .get('/items')
+        .then(response => {
+            console.log(response.body);
+        })
+        .catch(error => {
+            expect.fail(error)
+        })
+     })*/
+
+  })
+
+  //-------------- Test for DELETE-----------------------------------
+
+  describe('Delete first item', function() {
+    after(function() {
+        server.stop();
+      })
+    it('Should delete first item', async function() {
+      await chai.request(apiAddress)
+        .delete(putAddres)
+        .send({
+        })
+        .then(response => {
+          expect(response.status).to.equal(200);
+          return chai.request(apiAddress).get('/slots');
+        })
+        .catch(error => {
+          expect.fail(error)
+        })
+    })
+    // Uncommet if you want to see items printed and see if test really works
+    /*it("Print all items", async function() {
+        await chai.request(apiAddress)
+        .get('/items')
+        .then(response => {
+            console.log(response.body);
+        })
+        .catch(error => {
+            expect.fail(error)
+        })
+     })*/
+  })
